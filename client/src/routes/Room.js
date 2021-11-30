8b7cdce4902fd3bb6055a0bd8957b30c3c359268
lahiru <laahirudulanjaya@gmail.com>
2021-11-30 21:36:37 +0530

    change ice servers

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -121,10 +121,7 @@ const Room = (props) => {
             config: {
                 iceServers: [
                     {
-                        urls: [
-                            'stun:stun1.l.google.com:19302',
-                            'stun:stun2.l.google.com:19302',
-                        ],
+                        urls: 'stun:stun1.l.google.com:19302'
                     }]
             },
             stream,
